0318741ccdfc67e0f3841aa5aae5c1c56f0697f1
seth <coinlawyer20757@gmail.com>
2021-08-20 12:26:17 +0100

    New 4096x tileset image for source and new tile

--- src/PetesTiledExperiments.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="DungeonTilesetSprite" tilewidth="8" tileheight="8" tilecount="120" columns="8">
- <image source="Itch release raw tileset 01.png" trans="898989" width="64" height="120"/>
-</tileset>
---
+++ src/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="64" tileheight="64" tilecount="4096" columns="64">
+ <image source="tileset.png" width="4096" height="4096"/>
+</tileset>
